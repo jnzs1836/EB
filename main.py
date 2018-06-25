07b37a792916d79b3f61e851aa4fd0577a9dfbc9
jnzs1836 <wuguandejn@gmail.com>
2018-06-25 03:14:58 -0600

    goood

--- main.py
+++ main.py
@@ -2372,5 +2372,5 @@ def stock_info_2():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5200, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
     # app.run(host="0.0.0.0", ssl_context='adhoc')
